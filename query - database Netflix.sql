eb855435f799e0693f773f43e32dd843fa1dff1f
mcamparone0222 <mcamparone@gmail.com>
2024-08-05 14:57:33 -0300

    Add files via upload

---
+++ query - database Netflix.sql
@@ -0,0 +1,5 @@
+select * from [Netflix Data]
+
+--previamente se tuvieron que realizar varias conversiones, cambio de formato y arrgle de nomenclaturas de paises
+--adecuaciòn de caracteres invalidos y realizar un fill y reasearch de campos vacios
+
